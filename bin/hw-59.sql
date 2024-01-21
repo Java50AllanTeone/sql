4d521ea33475dd335fbd06e232bf0dd585d2d458
allanteone <allanteone@gmail.com>
2024-01-21 18:16:29 +0200

    hw-59

---
+++ bin/hw-59.sql
@@ -0,0 +1,5 @@
+--select SUBSTR(phone, 0, 4) from car_owners where city='city1';
+--select * from models where engine_power > engine_capacity * 0.06;
+--select name, phone from car_owners WHERE EXTRACT(MONTH FROM birth_date) = 12;
+--select number, model_name, model_year from cars where kilometers > 5000 and state='good';
+--select car_number, date from trade_deals where extract(MONTH FROM date) between 3 and 5;
